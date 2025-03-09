e7b779815e1a4f160e12d03d58f66fa4acc7f0fa
khaineswewynn <khaineshwewynn@gmail.com>
2025-03-09 11:46:40 +0630

    added heading

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -3,6 +3,7 @@ function App()
   return (
     <>
     <div>
+      <h2>Application</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, quis doloribus voluptate esse totam vel officia autem ullam, perferendis beatae, nisi ipsum ab illum rerum a ut voluptatem deserunt fuga?</p>
     </div>
     </>
