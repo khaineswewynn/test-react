4495dcea101a78ea9aed9b29059e55a44cbe8a8b
khaineswewynn <khaineshwewynn@gmail.com>
2025-03-09 11:48:12 +0630

    added another para

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -6,6 +6,10 @@ function App()
       <h2>Application</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, quis doloribus voluptate esse totam vel officia autem ullam, perferendis beatae, nisi ipsum ab illum rerum a ut voluptatem deserunt fuga?</p>
     </div>
+    <div>
+      <h2>another paragraph</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem cum quos, voluptatum autem fugiat cupiditate suscipit ex quaerat repudiandae a vero aliquid blanditiis laborum officiis iusto voluptate, eius rerum qui?</p>
+    </div>
     </>
   )
 }
